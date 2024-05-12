c19c7b0980676c6969a040e838c8ad474bd9ed0c
Eddie Lin <eddielin0926@gmail.com>
2024-05-12 14:22:56 +0200

    refactor: :building_construction: adjust project structure

---
+++ bun.build.ts
@@ -0,0 +1,54 @@
+import { Command } from "commander";
+import { watch } from "fs";
+
+type BuildConfig = {
+  publicDir: string;
+  sourceDir: string;
+  outDir: string;
+  sourceMap?: "none" | "inline" | "external";
+};
+
+const build = async (config: BuildConfig) =>
+  Bun.build({
+    entrypoints: ["./src/background/*.ts", "./src/content_scripts/*.ts"],
+    outdir: config.outDir,
+    target: "browser",
+    sourcemap: config.sourceMap ?? "none",
+    minify: true,
+  });
+
+const program = new Command();
+program
+  .option("--watch", "Watch for file changes", false)
+  .option("--source <dir>", "Source directory", "src")
+  .option("--out <dir>", "Output directory", "dist");
+program.parse(process.argv);
+const options = program.opts();
+
+const buildConfig: BuildConfig = {
+  publicDir: "public",
+  sourceDir: options.source,
+  outDir: options.out,
+};
+
+if (options.watch) {
+  console.log("Watching for file changes (Ctrl-C to exit)");
+  const watcher = watch(
+    options.source,
+    { recursive: true },
+    async (event, filename) => {
+      console.log(`Detected ${event} in ${filename}`);
+      await build({ ...buildConfig, sourceMap: "inline" });
+      console.log("Build complete");
+    }
+  );
+  process.on("SIGINT", () => {
+    console.log("Closing watcher...");
+    watcher.close();
+    process.exit(0);
+  });
+} else {
+  console.log("Building...");
+  build(buildConfig);
+  console.log("Build complete");
+}
---
+++ src/manifest.ts
@@ -0,0 +1,31 @@
+import packageJson from "../package.json";
+
+const manifest: chrome.runtime.Manifest = {
+  manifest_version: 3,
+  name: packageJson.name,
+  author: packageJson.author,
+  version: packageJson.version,
+  description: packageJson.description,
+  icons: {
+    "16": "icons/icon-16.png",
+    "48": "icons/icon-48.png",
+    "128": "icons/icon-128.png",
+  },
+  permissions: ["tabs", "unlimitedStorage", "storage"],
+  background: {
+    service_worker: "scripts/background.js",
+  },
+  content_scripts: [
+    {
+      matches: [
+        "https://leetcode.com/*",
+        "https://github.com/*",
+        "https://practice.geeksforgeeks.org/*",
+      ],
+      js: ["scripts/leetcode.js", "scripts/authorize.js", "scripts/gfg.js"],
+      run_at: "document_idle",
+    },
+  ],
+};
+
+export default manifest;
